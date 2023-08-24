6884e23a33a98ce4e1067eec2656807dccbf3764
Medinatu <medinatuali@gmail>
2023-08-24 12:31:53 -0700

    string

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
-#include "main.h"
-/**
-*print_sign-prints sign of a number
-*@n: character to be verified
-*Return: return 0 or 1
-*/
-int print_sign(int n)
+#include <stdio.h>
+
+int main(void)
 {
-if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
